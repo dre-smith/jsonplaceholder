052f37d5de3cb4fa7e7edc0626147443d762223c
Andrey <steamprofile2020@gmail.com>
2021-11-19 20:02:38 +0300

    form processing

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (request, response) => {
+    response.render('index')
+})
+
+app.post('/', (request, response) => {
+    const { city } = request.body
+    console.log(city)
     response.render('index')
 })
 
